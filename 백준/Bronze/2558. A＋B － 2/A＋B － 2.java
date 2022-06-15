ad904dbac7e24d038a36b1d4d5913a81606170df
Be-The-Key <rlfbd5142@gmail.com>
2022-06-15 21:55:59 +0900

    [Bronze V] Title: A+B - 2, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int lhs = sc.nextInt();
+		sc.nextLine();
+		int rhs = sc.nextInt();
+		System.out.println(lhs + rhs);
+	}
+}
